795f9ee66ff60e46fb13132805490dfecb28326a
Josep-11281 <11281@utl.edu.mx>
2023-11-30 18:42:35 -0600

    Pryecto

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
+
+
+
+
+
+
+
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>Hola</Text>
+      <Text style={styles.subtitulo}>Inicia Sesion</Text>
+      <TextInput
+       placeholder='Escribe tu correo'
+       style={styles.textInput}
+       
+      />
+      <TextInput 
+       placeholder='Escribe tu contraseña '
+       style={styles.textInput}
+
+      /> 
+      <Button style={styles.titulo}>Hola</Button>
+
       <StatusBar style="auto" />
     </View>
+
+    
   );
 }
 
@@ -17,4 +40,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    fontSize: 80,
+    color: '#228b22',
+    fontWeight: 'bold',
+  },
+
+  subtitulo: {
+    fontSize: 20,
+    color: '#228b22',
+  },
+  textInput: {
+    padding: 10,
+    paddingStart: 30,
+    width: '80%',
+    height: 50,
+    marginTop: 20,
+    borderRadius: 30,
+    backgroundColor: '#f8f8ff',
+    color: '#ff8c00',
+  }
+
+
 });
